c06ab2e006448447ec31b605131427b7d6b1d922
Anderson Sipriano <Andsipriano@gmail.com>
2024-08-24 17:24:54 -0300

    Understanding @PostConstruct Annotation

---
+++ example6/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,33 @@
+package com.example.beans;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+
+    private String name;
+
+    public Vehicle() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @PostConstruct
+    public void initialize() {
+        this.name = "honda";
+    }
+
+    public void printHello(){
+        System.out.println(
+                "Printing Hello from Component Vehicle Bean");
+    }
+
+}
