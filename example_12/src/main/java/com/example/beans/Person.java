7ea236855999ed70b89db8f7a05891e645ee5a7a
Anderson Sipriano <Andsipriano@gmail.com>
2024-08-28 17:29:18 -0300

    Deep dive of Autowiring inside Spring - Coding example

---
+++ example_12/src/main/java/com/example/beans/Person.java
@@ -0,0 +1,35 @@
+package com.example.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+    private String name="Lucy";
+    private Vehicle vehicle;
+
+    @Autowired
+    public Person(@Qualifier("vehicle1") Vehicle vehicle){
+        System.out.println("Person bean created by Spring");
+        this.vehicle = vehicle;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+}
